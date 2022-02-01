950d078f04abcb3cb78f6c9bcaa43b5112078e3e
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-02-01 15:36:57 +0100

    Updated the database tables, from pincode from messages to course

---
+++ steg1/mysql_db/CourseInsertStatements.sql
@@ -0,0 +1,4 @@
+INSERT INTO course (course_id, course_name) VALUES ("ITM30617","Utvikling av interaktive nettsteder"); 
+INSERT INTO course (course_id, course_name) VALUES ("ITF15019","Innføring i datasikkerhet"); 
+INSERT INTO course (course_id, course_name) VALUES ("BVN13092","Utvikling av interaktive bavianer"); 
+INSERT INTO course (course_id, course_name) VALUES ("OKS12032","Innføring i okse"); 
