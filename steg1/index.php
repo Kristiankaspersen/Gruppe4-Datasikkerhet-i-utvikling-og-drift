705c8d9c3f0491afe5f3b6683a42e5629948de44
vetlewj <vetlewj@gmail.com>
2022-02-01 23:04:39 +0100

    index page that links to overview of courses and registration page

--- steg1/index.php
+++ steg1/index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Steg1-Gruppe4</title>
 </head>
 <body>
-    <?php echo "<h1>Hei</h1><br><a href="steg1">Gå til steg 1</a>" ?>
+<h1><a href="auth/index.php">Registrering / Innlogging</a></h1>
+<h2><a href="courses.php">Alle emner</a></h2>
 </body>
 </html>
