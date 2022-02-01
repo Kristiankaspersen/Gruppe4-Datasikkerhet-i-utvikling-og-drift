94749b528bf4ec9fe606e89985c1f1733f8d5fbe
Nettking <m@nettking.no>
2022-02-01 13:21:38 +0100

    Lager dummy api

--- steg1/api/index.php
+++ steg1/api/index.php
@@ -1,3 +1,12 @@
+<html>
+<form action="" method="POST">
+<label>Enter Order ID:</label><br />
+<input type="text" name="order_id" placeholder="Enter Order ID" required/>
+<br /><br />
+<button type="submit" name="submit">Submit</button>
+</form>
+</html>
 <?php
 echo "API-side!";
+
 ?> 
