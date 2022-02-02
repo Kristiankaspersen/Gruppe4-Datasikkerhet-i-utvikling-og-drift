ecb19aa8f6040756756a4e78d8680f162f34ec45
vetlewj <vetlewj@gmail.com>
2022-02-02 22:41:39 +0100

    todo pseudocode for course page

--- steg1/course.php
+++ steg1/course.php
@@ -1,3 +1,6 @@
+<?php 
+ session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +22,14 @@ $mysqli = new mysqli($servername, $username, $password, $dbName);
 $query = "SELECT course_id, course_name FROM course";
 
 echo $_GET['course'];
+echo $_SESSION["username"];
+
+// TODO
+// coruse code on top, can be retrieved with $_GET['course'] (param in url)
+// if logged in user is lecturer in course, display all messages, option to reply to messages
+// else if access through pin, display all messages, option to comment on messages
+// else display message
+
 ?>
 <h2><a href=""></a></h2>
 </body>
