350e191dc60309625e093d7ed1b618d036aea774
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-02-04 11:53:40 +0100

    the api create for lecturer is now working

--- steg1/models/Lecturer.php
+++ steg1/models/Lecturer.php
@@ -18,6 +18,9 @@ class Lecturer extends User {
             case 8: 
                 $this->construct8($args[0], $args[1], $args[2], $args[3], $args[4], $args[5], $args[6], $args[7]); 
             break; 
+            case 9: 
+                $this->construct9($args[0], $args[1], $args[2], $args[3], $args[4], $args[5], $args[6], $args[7], $args[8]);
+            break;
             default: 
                 trigger_error("Incorrect number of arguments for Lecturer::__construct",  E_USER_WARNING);
         }
@@ -39,6 +42,18 @@ class Lecturer extends User {
         
     }
 
+    private function construct9($db, $username, $firstName, $lastName,  $email, $password, $passwordRepeat, $profilePictureAdress, $courseID)
+    {
+        parent::__construct($username, $firstName, $lastName,  $email, $password, $passwordRepeat);
+        
+        $this->profilePictureAdress = $profilePictureAdress; 
+        $this->courseID = $courseID; 
+
+        $this->conn = $db; 
+        $this->role = 'lecturer'; 
+        
+    }
+
     public function read() {
         // create query 
         $query = "SELECT  *
@@ -50,6 +65,68 @@ class Lecturer extends User {
         $stmt->execute(); 
         
         return $stmt; 
+    }
+
+    // Create lecturer 
+    public function create() {
+
+        $insertUserTable = $this->conn->prepare('INSERT INTO user (username, first_name, last_name, email, password, user_role ) VALUES (?, ?, ?, ?, ?, ?); ');
+        $insertLecturerTable = $this->conn->prepare('INSERT INTO lecturer (lecturer_id, profilepicture, course_course_id) VALUES (? ,?, ?); ');
+        $insertLecturerHasUserTable = $this->conn->prepare('INSERT INTO lecturer_has_user (lecturer_lecturer_id, user_username) VALUES (?, ?)'); 
+
+        $lecturerID = $this->conn->query('SELECT COUNT(*) FROM student;')->fetchAll(PDO::FETCH_ASSOC);
+        
+        // Doing it like this, will not work if students are deleted in DB. 
+        $lecturerID = $lecturerID[0]["COUNT(*)"];
+        
+        // Checks if student_id exists: 
+        $checkIfLecturerIdExists = null; 
+        do {
+            $lecturerID +=1;
+            $checkIfLecturerIdExists = $this->conn->query("SELECT student_id FROM student WHERE student_id = $lecturerID")->fetchAll(PDO::FETCH_ASSOC);
+            $checkIfLecturerIdExists = $checkIfLecturerIdExists[0]["student_id"];
+
+        } while($checkIfLecturerIdExists != null); 
+
+        // $username, $firstName, $lastName,  $email, $password, $passwordRepeat, $fieldOfStudy, $startingYear
+
+        // Clean data
+        $this->username = htmlspecialchars(strip_tags($this->username)); 
+        $this->firstName = htmlspecialchars(strip_tags($this->firstName)); 
+        $this->lastName = htmlspecialchars(strip_tags($this->lastName)); 
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->profilePictureAdress = htmlspecialchars(strip_tags($this->profilePictureAdress));
+        $this->courseID = htmlspecialchars(strip_tags($this->courseID));
+         
+        $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT); 
+
+        // have to do a transaction here or something In case of any of the executions goes wrong. 
+        if(!$insertUserTable->execute(array($this->username, $this->firstName, $this->lastName, $this->email, $hashedPassword, $this->role))) {
+                printf("Error: %s. \n", $insertUserTable->error);
+                $insertUserTable = null; 
+                return false; 
+        } 
+        if(!$insertLecturerTable->execute(array($lecturerID, $this->profilePictureAdress, $this->courseID))) {
+                printf("Error: %s. \n", $insertLecturerTable->error);
+                $insertLecturerTable = null; 
+                
+                return false;  
+        } 
+        if(!$insertLecturerHasUserTable->execute(array($lecturerID, $this->username))) {
+                printf("Error: %s. \n", $insertLecturerHasUserTable->error);
+                $insertLecturerHasUserTable = null;
+                
+                return false; 
+        }
+        
+
+        $insertUserTable = null; 
+        $insertLecturerTable = null; 
+        $insertLecturerHasUserTable = null;
+
+        return true; 
+    
     }
 
 
