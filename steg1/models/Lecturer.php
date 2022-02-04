0e5cde478ab80cd985803b41b80efc3520445646
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-02-04 14:00:22 +0100

    almost finished with delete in api for lecturer

--- steg1/models/Lecturer.php
+++ steg1/models/Lecturer.php
@@ -2,7 +2,8 @@
 
 class Lecturer extends User { 
     private $profilePictureAdress; 
-    private $courseID; 
+    private $courseID;
+    private $lecturerID;  
 
     // I have to do it like this to overload the constructor. 
     function __construct() {
@@ -20,6 +21,9 @@ class Lecturer extends User {
             break; 
             case 9: 
                 $this->construct9($args[0], $args[1], $args[2], $args[3], $args[4], $args[5], $args[6], $args[7], $args[8]);
+            break;
+            case 10: 
+                $this->construct10($args[0], $args[1], $args[2], $args[3], $args[4], $args[5], $args[6], $args[7], $args[8], $args[9]);
             break;
             default: 
                 trigger_error("Incorrect number of arguments for Lecturer::__construct",  E_USER_WARNING);
@@ -48,6 +52,19 @@ class Lecturer extends User {
         
         $this->profilePictureAdress = $profilePictureAdress; 
         $this->courseID = $courseID; 
+
+        $this->conn = $db; 
+        $this->role = 'lecturer'; 
+        
+    }
+
+    private function construct10($db, $username, $firstName, $lastName,  $email, $password, $passwordRepeat, $profilePictureAdress, $courseID, $lecturerID)
+    {
+        parent::__construct($username, $firstName, $lastName,  $email, $password, $passwordRepeat);
+        
+        $this->profilePictureAdress = $profilePictureAdress; 
+        $this->courseID = $courseID;
+        $this->lecturerID = $lecturerID;  
 
         $this->conn = $db; 
         $this->role = 'lecturer'; 
@@ -128,6 +145,93 @@ class Lecturer extends User {
         return true; 
     
     }
+
+    // Create lecturer 
+    public function update() {
+
+        $insertUserTable = $this->conn->prepare('INSERT INTO user (username, first_name, last_name, email, password, user_role ) VALUES (?, ?, ?, ?, ?, ?); ');
+        $insertLecturerTable = $this->conn->prepare('INSERT INTO lecturer (lecturer_id, profilepicture, course_course_id) VALUES (? ,?, ?); ');
+        $insertLecturerHasUserTable = $this->conn->prepare('INSERT INTO lecturer_has_user (lecturer_lecturer_id, user_username) VALUES (?, ?)'); 
+
+
+        // $username, $firstName, $lastName,  $email, $password, $passwordRepeat, $fieldOfStudy, $startingYear
+
+        // Clean data
+        $this->username = htmlspecialchars(strip_tags($this->username)); 
+        $this->firstName = htmlspecialchars(strip_tags($this->firstName)); 
+        $this->lastName = htmlspecialchars(strip_tags($this->lastName)); 
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->profilePictureAdress = htmlspecialchars(strip_tags($this->profilePictureAdress));
+        $this->courseID = htmlspecialchars(strip_tags($this->courseID));
+        $this->lecturerID = htmlspecialchars(strip_tags($this->lecturerID));
+        $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT); 
+
+        // have to do a transaction here or something In case of any of the executions goes wrong. 
+        if(!$insertUserTable->execute(array($this->username, $this->firstName, $this->lastName, $this->email, $hashedPassword, $this->role))) {
+                printf("Error: %s. \n", $insertUserTable->error);
+                $insertUserTable = null; 
+                return false; 
+        } 
+        if(!$insertLecturerTable->execute(array($this->lecturerID, $this->profilePictureAdress, $this->courseID))) {
+                printf("Error: %s. \n", $insertLecturerTable->error);
+                $insertLecturerTable = null; 
+                
+                return false;  
+        } 
+        if(!$insertLecturerHasUserTable->execute(array($this->lecturerID, $this->username))) {
+                printf("Error: %s. \n", $insertLecturerHasUserTable->error);
+                $insertLecturerHasUserTable = null;
+                
+                return false; 
+        }
+        
+
+        $insertUserTable = null; 
+        $insertLecturerTable = null; 
+        $insertLecturerHasUserTable = null;
+
+        return true; 
+    
+    }
+
+        // delete lecturer 
+        public function delete() {
+
+            $insertLecturerHasUserTable = $this->conn->prepare('DELETE FROM lecturer_has_user WHERE lecturer_lecturer_id = :lecturer_lecturer_id AND user_username = :username');
+            $insertLecturerTable = $this->conn->prepare('DELETE FROM lecturer  WHERE lecturer_id = :lecturer_id');
+            $insertUserTable = $this->conn->prepare('DELETE FROM user WHERE username = :username');
+
+            // Clean data
+            $this->username = htmlspecialchars(strip_tags($this->username)); 
+            $this->lecturerID = htmlspecialchars(strip_tags($this->lecturerID));
+        
+    
+            // have to do a transaction here or something In case of any of the executions goes wrong. 
+            if(!$insertLecturerHasUserTable->execute(array($this->lecturerID, $this->username))) {
+                printf("Error: %s. \n", $insertLecturerHasUserTable->error);
+                $insertLecturerHasUserTable = null;
+                
+                return false; 
+            }
+            if(!$insertLecturerTable->execute(array($this->lecturerID))) {
+                printf("Error: %s. \n", $insertLecturerTable->error);
+                $insertLecturerTable = null; 
+                
+                return false;  
+        } 
+            if(!$insertUserTable->execute(array($this->username))) {
+                    printf("Error: %s. \n", $insertUserTable->error);
+                    $insertUserTable = null; 
+                    return false; 
+            } 
+            
+            $insertUserTable = null; 
+            $insertLecturerTable = null; 
+            $insertLecturerHasUserTable = null;
+    
+            return true;
+        }
 
 
 
