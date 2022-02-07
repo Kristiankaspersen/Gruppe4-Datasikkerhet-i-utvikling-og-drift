c4a13b4cd9220517e324e8ccff4cf4fd738a498e
Kimkopp <kimkop1@hotmail.com>
2022-02-07 17:05:51 +0100

    yes

--- steg1/auth/Change_Password.php
+++ steg1/auth/Change_Password.php
@@ -1,8 +1,10 @@
 <?php 
 session_start();
 
-if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {}
-?>
+if (isset($_SESSION['student_id']) || isset($_SESSION["lecture_id"]) && isset($_SESSION['username'])) 
+{
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,28 +14,42 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {}
 </head>
 <body>
     <h2 align="center"> Change Password</h2>
-  <div>  <?php if(isset($message)) {echo $message;} ?> </div>
-<form method="post" action="classes/C-Password.php" align="center">
+    <?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+     	<?php if (isset($_GET['success'])) { ?>
+            <p class="success"><?php echo $_GET['success']; ?></p>
+        <?php } ?>
+<form method="post" action="C_Password.php" align="center">
 Current Password:<br>
 <input type="password" 
-name="cpwd">
-<span id="cpwd"></span>
+name="op">
+<span id="op"></span>
 <br>
 
 New Password:<br>
 <input type="password" 
-name="npwd">
-<span id="npwd" ></span>
+name="np">
+<span id="np" ></span>
 <br>
 
 Confirm Password:<br>
 <input type="password" 
-name="c_npwd">
-<span id="c_npwd" ></span>
+name="c_np">
+<span id="c_np" ></span>
 <br><br>
 <input type="submit">
+<br>
+<a href="index.php" class="ca">HOME</a>
 </form>
 
 </body>
 
 </html>
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
