5286a8670f401fb913e5a2e874ed109eb9c39ee6
kristian kaspersen <kristkas@hiof.no>
2022-04-05 13:53:10 +0000

    fix logging

--- steg2/mysql_db/dropAndCreateDB.php
+++ steg2/mysql_db/dropAndCreateDB.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost:8889";
-$username = "root";
-$password = "root";
+$servername = "localhost";
+$username = "fire_db_bruker";
+$password = "NF@Ykpk7M8b3";
 
 // Creating connection
 $conn = mysqli_connect($servername, $username, $password);
